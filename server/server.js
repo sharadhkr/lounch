460a9af85feeb3b649d6296211f83464901522ee
sharadhkr <www.sharadrathoreji@gmail.com>
2025-04-12 09:01:06 +0530

    Re-add category.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000
 const server = http.createServer(app);
+const fs = require('fs');
+console.log('Files in models folder:', fs.readdirSync(__dirname + '/models'));
 
 
 server.listen(port, () =>{
