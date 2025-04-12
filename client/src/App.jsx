a6b1a131ee393cdb9bd8e23940909e7a6cfd6f12
sharadhkr <www.sharadrathoreji@gmail.com>
2025-04-12 09:47:27 +0530

    fdef

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import BottomNavbar from "./components/Navbar";
+import BottomNavbar from "./components/BottomNavbar";
 import Bottom from "./components/Bottom";
 
 
